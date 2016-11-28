5c546c5b49f80c0f3736d080f72e565a1784a99d
vinaysrikanta <vinaysrikanta@gmail.com>
2016-11-28 15:31:52 -0500

    Add files via upload

---
+++ HelloJavaWorld.java
@@ -0,0 +1,10 @@
+package com.cognizant.training;
+
+public class HelloJavaWorld {
+	
+	public static void main (String args[]) {
+		
+		System.out.println("My Firsgt Jenkins Java Programme RUN ");
+	}
+
+}
